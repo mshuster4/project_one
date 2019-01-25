2a9661b95f43de74ea92bfd15a3b28343c461a54
Emroze <emrozesd@users.noreply.github.com>
2019-01-24 20:06:18 -0800

    changed labels

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-var test;
+
+
+function displayBrewerys() {
+
+
+        var city = $("#city-name").val().trim(); 
+
+        var state = $("#state-name").val().trim(); 
+
+        var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city; 
+
+            $.ajax({
+
+            url: queryURL,
+            method: "GET"
+
+            })
+
+            .then(function(response) {
+
+            console.log(response); 
+
+            });
+
+    }
+
+
+$("#submitSearch").on("click", displayBrewerys()); 
