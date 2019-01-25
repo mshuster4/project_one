35f7e8103330cd42cb0225c7ee32a538f16d5e00
Emroze <emrozesd@users.noreply.github.com>
2019-01-24 20:22:18 -0800

    added AJAX call

--- javascript.js
+++ javascript.js
@@ -2,27 +2,31 @@
 
 function displayBrewerys() {
 
+    var city = $("#city-name").val().trim(); 
 
-        var city = $("#city-name").val().trim(); 
+    var state = $("#state-name").val().trim(); 
 
-        var state = $("#state-name").val().trim(); 
+    var queryURL = "https://api.openbrewerydb.org/breweries?by_state=" + state + "&by_city=" + city; 
 
-        var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city; 
+        $.ajax({
 
-            $.ajax({
+        url: queryURL,
+        method: "GET"
 
-            url: queryURL,
-            method: "GET"
+        })
 
-            })
+        .then(function(response) {
 
-            .then(function(response) {
+        console.log(response); 
 
-            console.log(response); 
+        });
 
-            });
+}
 
-    }
+$(document).on("click", "#submitSearch", function() {
 
+    event.preventDefault();
 
-$("#submitSearch").on("click", displayBrewerys()); 
+    displayBrewerys(); 
+
+});
