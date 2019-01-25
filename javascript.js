dfb123de8d2b9f89af1b7ca8d6f83204b7a8c53b
Kara Nichols <lnicholskara@gmail.com>
2019-01-24 18:58:38 -0800

    basic API call - no filters

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
-var test;
+function getTicketmasterData () {
+
+    
+
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=QZRsgKsNDgOLAvshwXSGPyRAHB3ImEda"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+        console.log (response);
+      });
+
+}
+
+$("#submitSearch").on("click", getTicketmasterData())
